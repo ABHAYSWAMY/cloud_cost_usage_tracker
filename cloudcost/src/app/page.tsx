746c9f6f12393417d93630451b3a1dc718ed0779
ABHAYSWAMY <abhayswamy9@gmail.com>
2026-01-20 14:20:05 +0530

    phase 0 setup done

---
+++ cloudcost/src/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <main className="p-8">
+      <h1 className="text-2xl font-semibold">
+        Cloud Cost & Usage Analyzer
+      </h1>
+    </main>
+  );
+}
